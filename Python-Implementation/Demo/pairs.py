185b643ea7a0a6be70ec9316fe1141d7fb7cdd34
Rohit Sinha <rohit.k.sinha@oracle.com>
2021-05-21 19:40:34 +0530

    Added comments in pair_sum python implementation for better understanding

--- Python-Implementation/Demo/pairs.py
+++ Python-Implementation/Demo/pairs.py
@@ -1,14 +1,21 @@
 def pair_sum(num_list, target_sum):
-    """Returns a tuple of pair of numbers from a list of unsorted numbers that sums to target_sum in linear time"""
+    """Returns a tuple of pair of numbers
+    from a list of unsorted numbers
+    that sums to target_sum in linear time.
+    Had the list been sorted,
+    we could have even used two pointer method."""
 
     # initializing lookup set
     complement_lookup = set()
 
     for num in num_list:
+        # Look up additive inverse/complement of the number from the set
+        # If found, return the pair, else add the number to the lookup set
         complement = target_sum-num
         if complement in complement_lookup:
             return complement, num
         else:
             complement_lookup.add(num)
 
+    # If no such pair is found, return -1
     return -1
